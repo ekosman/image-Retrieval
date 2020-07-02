e1b1f5c826f380be4d491579c35978e178a214dc
Eitan <eitan.kosman@gmail.com>
2020-07-02 19:00:05 +0300

    add code and images

--- vgg_utils.py
+++ vgg_utils.py
@@ -11,8 +11,8 @@ class VGG:
 		# if pretrained_path is not None:
 		# 	model.load_state_dict(load('checkpoint/vgg16/vgg16-190-best.pth', map_location='cpu'))
 		#
-		# model.classifier = nn.Sequential(*list(model.classifier.children())[:3])
-		self.model = nn.Sequential(*list(model.features.children())[:2])
+		model.classifier = nn.Sequential(*list(model.classifier.children())[:3])
+		# self.model = nn.Sequential(*list(model.features.children())[:2])
 		if use_cuda:
 			self.model = self.model.cuda()
 
