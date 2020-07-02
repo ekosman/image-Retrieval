ce5a75dc8a1366f950ddc7a8ce37b5ab8a583a27
Eitan <eitan.kosman@gmail.com>
2020-07-02 18:56:27 +0300

    add code and images

--- vgg_utils.py
+++ vgg_utils.py
@@ -5,13 +5,13 @@ from torchvision import transforms, models
 class VGG:
 	def __init__(self, use_cuda=False, pretrained_path=None):
 		self.use_cuda = use_cuda
-		model = models.vgg16_bn(pretrained=False)
-		num_ftrs = model.classifier[6].in_features
-		model.classifier[6] = nn.Linear(num_ftrs, 100)
-		if pretrained_path is not None:
-			model.load_state_dict(load('checkpoint/vgg16/vgg16-190-best.pth', map_location='cpu'))
-
-		model.classifier = nn.Sequential(*list(model.classifier.children())[:3])
+		model = models.vgg16_bn(pretrained=True)
+		# num_ftrs = model.classifier[6].in_features
+		# model.classifier[6] = nn.Linear(num_ftrs, 100)
+		# if pretrained_path is not None:
+		# 	model.load_state_dict(load('checkpoint/vgg16/vgg16-190-best.pth', map_location='cpu'))
+		#
+		# model.classifier = nn.Sequential(*list(model.classifier.children())[:3])
 		self.model = nn.Sequential(*list(model.features.children())[:2])
 		if use_cuda:
 			self.model = self.model.cuda()
