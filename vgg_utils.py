c5a2ec8856558078b6eebc9318f937fc4d08d5e4
Eitan <eitan.kosman@gmail.com>
2020-07-18 18:08:58 +0300

    CHANGE use vgg19 instead of vgg16

--- vgg_utils.py
+++ vgg_utils.py
@@ -1,19 +1,15 @@
 from torch import nn, load
 from torchvision import transforms, models
-
+from torchvision.models.vgg import model_urls
 
 class VGG:
 	def __init__(self, use_cuda=False, pretrained_path=None):
 		self.use_cuda = use_cuda
-		model = models.vgg16_bn(pretrained=True)
-		# num_ftrs = model.classifier[6].in_features
-		# model.classifier[6] = nn.Linear(num_ftrs, 100)
-		# if pretrained_path is not None:
-		# 	model.load_state_dict(load('checkpoint/vgg16/vgg16-190-best.pth', map_location='cpu'))
-		#
+		model_urls['vgg19'] = model_urls['vgg19'].replace('https://', 'http://')
+		model = models.vgg19(pretrained=True, progress=True)
+
 		model.classifier = nn.Sequential(*list(model.classifier.children())[:3])
 		self.model = model
-		# self.model = nn.Sequential(*list(model.features.children())[:2])
 		if use_cuda:
 			self.model = self.model.cuda()
 
